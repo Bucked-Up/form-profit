c6af816ece9a7dc80a13c19524c2376ce7f0826d
deigo-lps <diego.lopes0909@gmail.com>
2024-07-01 13:55:12 -0300

    changes id to form-wholesale

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 
-const form = document.querySelector("#custom-form");
+const form = document.querySelector("#form-wholesale");
 const allInputs = form.querySelectorAll("input");
 
 const createInvalid = (text) => {
